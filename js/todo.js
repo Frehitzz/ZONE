d63f4789ce544b75c715265f5574cc163f7b1198
Frehitzz <fritzharlydegamo@gmail.com>
2025-08-11 07:22:54 +0800

    Implement an addbutton,cancelbutt,and show notes

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,22 @@
 function addbutt(){
+    const todoModal = document.querySelector('.todo-modal');
+
+    todoModal.style.display = 'flex';
+}
+
+function cancelbutt(){
+    const todoModal = document.querySelector('.todo-modal');
+    todoModal.style.display = 'none';
+}
+
+function showNotes(){
+    const mynotes = document.querySelector('.notes');
+    const notesDisplay = getComputedStyle(mynotes).display;
+
+    if(notesDisplay === "none"){
+        mynotes.style.display = 'block';
+    }else{
+        mynotes.style.display = 'none';
+    }
     
 }
