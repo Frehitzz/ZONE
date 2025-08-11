d329fd8360529948f837cb469313c0e5029f4f92
Frehitzz <fritzharlydegamo@gmail.com>
2025-08-11 23:08:15 +0800

    implement a function to save the user todo's and display it on the container

--- js/todo.js
+++ js/todo.js
@@ -1,14 +1,36 @@
+// DISPLAY THE MODAL
 function addbutt(){
     const todoModal = document.querySelector('.todo-modal');
 
     todoModal.style.display = 'flex';
 }
 
+function savebutt(){
+    //GET THE USER INPUT
+    const mytodo = document.querySelector('.input-todo');
+    const mynotes = document.querySelector('.notes');
+    const usertodo = mytodo.value;
+    const usernote = mynotes.value;
+
+    //GET THE DIV ON HTML WHERE WE GON DISPLAY THE TASK
+    const displaytodo = document.querySelector('.display-todo');
+    //DISPLAYING THE TASK ON HTML
+    displaytodo.innerHTML += "<div class='added-task'>" + usertodo + mynotes + "</div>";
+
+    // AFTER SAVING THE INPUT WILL BE CLEAR
+    mytodo.value = "";
+
+    //CLOSING THE MODAL AFTER ADDED A TASK
+    cancelbutt();
+}
+
+// HIDE THE MODAL
 function cancelbutt(){
     const todoModal = document.querySelector('.todo-modal');
     todoModal.style.display = 'none';
 }
 
+// TO SHOW TEXTAREA 
 function showNotes(){
     const mynotes = document.querySelector('.notes');
     const notesDisplay = getComputedStyle(mynotes).display;
