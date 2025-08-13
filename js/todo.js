5e609f85e6c6978978cedb5fbc1c0567d3307ccf
Frehitzz <fritzharlydegamo@gmail.com>
2025-08-13 22:46:58 +0800

    FIXED: editing existing task and notes

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,7 @@
+let todos = []; // FOR STORING TASKS AND NOTES
+let todoIndex = null // TO TRACK WHAT TASK IS BEING EDITED
+
+
 // DISPLAY THE MODAL
 function addbutt(){
     const todoModal = document.querySelector('.todo-modal');
@@ -7,26 +11,83 @@ function addbutt(){
 
 function savebutt(){
     //GET THE USER INPUT
+    // INPUT CLASS FOR OUR TASK NAME
     const mytodo = document.querySelector('.input-todo');
+    // CLASS OF OUR TEXTARE
     const mynotes = document.querySelector('.notes');
 
+    // GETTING USER INPUT TASK NAME
     const usertodo = mytodo.value;
+    // GETTING USER NOTES
     const usernote = mynotes.value;
 
-    Userinput(usertodo, usernote);
+    // IF THE TODOS ARE NOT EMPTY
+    if(todoIndex !== null){
+        // "task" and "note" are only label
+        // THIS CODE IS FOR DISPLAYING THE SELECTED TASK INFO ABOUT NOTES AND TASK
+        todos[todoIndex] = {task: usertodo, note: usernote};
+        // IF YOU EDIT AND PUT NEW AND CHOOSE CANCEL THE INFO YOU PUT WILL NOT GON SAVE
+        todoIndex = null;
+    }else{
+        // THIS FOR DISPALYING THE NEW TASK 
+        todos.push({task: usertodo, note: usernote});
+    }
 
-    //CLOSING THE MODAL AFTER ADDED A TASK
-    cancelbutt();
+    renderTodos(); // UPDATE THE DISPLAY
+    cancelbutt(); //CLOSING THE MODAL AFTER ADDED A TASK
 
 }
 
-function Userinput(task, note){
-    //GET THE DIV ON HTML WHERE WE GON DISPLAY THE TASK
+function renderTodos(){
+    // DIV FOR ADDED TASK
     const displaytodo = document.querySelector('.display-todo');
-    //DISPLAYING THE TASK ON HTML
-    displaytodo.innerHTML += "<div class='added-task'>" + "<div class='tasksonly'>" + "<div class='usertask-container'>" + "<input type='checkbox' class='task-checkb'>" + task + "</div>" + "<div class='three-dot'>" + "<i class='fa-solid fa-ellipsis-vertical' onclick='editTask()'></i>" + "</div>" + "</div>" + "<div class='notes-container'>"+ note + "</div>" + "</div>";
-    // AFTER SAVING THE INPUT WILL BE CLEAR
+    displaytodo.innerHTML = "";
+
+    // foreach TO LOOP THROUGH EVERY TODO TASK 
+    todos.forEach((item,idx) => {  
+    displaytodo.innerHTML += `
+        <div class='added-task'>
+            <div class='tasksonly'>
+                <div class='usertask-container'>
+                    <input type='checkbox' class='task-checkb'>
+                    ${item.task}
+                </div>
+                <div class='three-dot'>
+                    <i class='fa-solid fa-ellipsis-vertical' onclick='editTask(${idx})'></i>
+                </div>
+            </div>
+            <div class='notes-container'>${item.note}</div>
+        </div>
+    `;
+    // item.task = SHOW THE TASK TEXT
+    // item.note = SHOW THE NOTE TEXT
+    });             
+
 }
+
+function editTask(idx) {
+    const todoModal = document.querySelector('.todo-modal');
+    const mytodo = document.querySelector('.input-todo');
+    const mynotes = document.querySelector('.notes');
+    const mycheckb = document.querySelector('.checkb');
+
+    // Fill modal with selected task/note
+    mytodo.value = todos[idx].task;
+    mynotes.value = todos[idx].note;
+
+    // Show notes textarea if note exists
+    if (todos[idx].note) {
+        mynotes.style.display = 'block';
+        mycheckb.checked = true;
+    } else {
+        mynotes.style.display = 'none';
+        mycheckb.checked = false;
+    }
+
+    todoIndex = idx; // Set which task is being edited
+    todoModal.style.display = 'flex';
+}
+
 
 // HIDE THE MODAL
 function cancelbutt(){
@@ -65,10 +126,5 @@ function showNotes(){
 
 }
 
-function editTask(){
-    addbutt();
 
-
-    
-}
      
